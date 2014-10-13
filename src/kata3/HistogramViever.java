ab8cb453c1d35f2e734fd2d3e120a5a425434f40
usuario <usuario@10.22.143.35>
2014-10-13 13:53:09 +0100

    Implementacion de la clase HistogramVieverr. metodo print() completo

--- src/kata3/HistogramViever.java
+++ src/kata3/HistogramViever.java
@@ -3,6 +3,8 @@ package kata3;
 public class HistogramViever <Type>{
     
     public void print(Histogram<Type> histogram){
-        
+        for (Type type : histogram.keySet()) {
+            System.out.println(type + " -> " + histogram.get(type));
+        }
     }
 }
