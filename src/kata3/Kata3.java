ee536bd55ed337d50932a358f83b79ab7ab97167
usuario <usuario@10.22.143.35>
2014-10-13 14:10:55 +0100

    Terminado el main

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -2,5 +2,9 @@ package kata3;
 
 public class Kata3 {
     public static void main(String[] args) {       
+        MailReader mailReader = new MailReader("mails.txt");
+        HistogramBuilder<String> builder = new HistogramBuilder();
+        Histogram<String> histogram = builder.build(mailReader.readDomain());
+        new HistogramViever<String>().print(histogram);
     }
 }
