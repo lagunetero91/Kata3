571a5b0b56acd42523c485ccd94e42e5a1c2a0bd
usuario <usuario@10.22.143.35>
2014-10-13 13:47:13 +0100

    Implementacion de la clase HistogramBuilder. metodo build() completo

--- src/kata3/HistogramBuilder.java
+++ src/kata3/HistogramBuilder.java
@@ -3,6 +3,14 @@ package kata3;
 public class HistogramBuilder <Type> {
     
     public Histogram<Type> build(Type[] types){
-        return null;
+        Histogram<Type> histogram = new Histogram <>();
+        for (Type type : types) {
+            if( histogram.containsKey(type))
+                histogram.put(type, histogram.get(type) + 1);
+            else
+                histogram.put(type, 1);
+        }
+                
+        return histogram;
     }
 }
