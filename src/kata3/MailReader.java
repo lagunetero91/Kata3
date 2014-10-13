427bdccdf6962564b949e01528b613156d57af01
usuario <usuario@10.22.143.35>
2014-10-13 13:33:12 +0100

    Implementacion de la clase MailReader. metodo readDomain()

--- src/kata3/MailReader.java
+++ src/kata3/MailReader.java
@@ -1,5 +1,10 @@
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MailReader {
     private final String filePath;
 
@@ -8,6 +13,17 @@ public class MailReader {
     }
     
     public String[] readDomain(){
-        return null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            ArrayList<String> domainList = new ArrayList <>();
+            reader.close();
+            return null;
+        } catch (IOException ex) {
+            try {
+                reader.close();
+            } catch (IOException ex1) {}
+            return new String[0];
+        }
     }
 }
