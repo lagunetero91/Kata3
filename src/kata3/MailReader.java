207cc744a4bab0b9ec8841444978c6c7d7e576da
usuario <usuario@10.22.143.35>
2014-10-13 13:17:58 +0100

    Añadida la clase MailReader

---
+++ src/kata3/MailReader.java
@@ -0,0 +1,13 @@
+package kata3;
+
+public class MailReader {
+    private final String filePath;
+
+    public MailReader(String filePatch) {
+        this.filePath = filePatch;
+    }
+    
+    public String[] readDomain(){
+        return null;
+    }
+}
