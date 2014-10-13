77b012b55245f4b014c2bbc6ea8d28c8a8a2ac9a
usuario <usuario@10.22.143.35>
2014-10-13 14:01:24 +0100

    añadimos metodo get() a la calse Histogram

--- src/kata3/HistogramBuilder.java
+++ src/kata3/HistogramBuilder.java
@@ -4,13 +4,8 @@ public class HistogramBuilder <Type> {
     
     public Histogram<Type> build(Type[] types){
         Histogram<Type> histogram = new Histogram <>();
-        for (Type type : types) {
-            if( histogram.containsKey(type))
+        for (Type type : types) 
                 histogram.put(type, histogram.get(type) + 1);
-            else
-                histogram.put(type, 1);
-        }
-                
         return histogram;
     }
 }
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,7 +1,6 @@
 package kata3;
 
 public class Kata3 {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
     }
 }
